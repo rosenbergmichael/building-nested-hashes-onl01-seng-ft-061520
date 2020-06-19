1606602b51b4fbd996338b0cfd7a0bd9b1e8e847
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 22:25:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,13 +5,12 @@ def second_challenge
     :matriarch => {},
     :hero => {},
     :hero_friends => []
-    }
-     
-      #your key/value pairs here!
-      }, 
+     }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+    :patriarch => {},
+    :matriarch => {},
+    :heroine => {},
+    :heroine_friends => []
+      }
+  }
 end
